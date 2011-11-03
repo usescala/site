15e7d7670bb76fbf60c006395c55b012f7920936
Erik Peterson <ep@ardec.com>
2011-11-03 15:59:14 -0600

    css, logo

--- project/plugins/build.sbt
+++ project/plugins/build.sbt
@@ -8,4 +8,4 @@ addSbtPlugin("com.github.siasia" % "xsbt-web-plugin" % "0.1.2")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.7.1")
 
-addSbtPlugin("com.zentrope" %% "xsbt-scalate-precompile-plugin" % "1.6")
+//addSbtPlugin("com.zentrope" %% "xsbt-scalate-precompile-plugin" % "1.6")
--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,5 +1,8 @@
 package usescala.site
 
+import org.fusesource.scalate.servlet.ServletTemplateEngine
+import org.fusesource.scalate.layout.DefaultLayoutStrategy
+
 class Service extends unfiltered.filter.Plan {
 	import unfiltered.request._
 	import unfiltered.response._
@@ -17,6 +20,13 @@ class Service extends unfiltered.filter.Plan {
 }
 
 object Server extends App {
+
+  val engine = new ServletTemplateEngine()
+  engine.layoutStrategy = new DefaultLayoutStrategy(engine,"templates/default.jade")
+
 	val port = util.Properties.envOrElse("PORT", "8080").toInt
-  unfiltered.jetty.Http(port).filter(new Service).run
+  unfiltered.jetty.Http(port)
+   .context("/web") { _.resources(new java.net.URL(getClass().getResource("/web/css"), ".")) }
+   .context("/web") { _.resources(new java.net.URL(getClass().getResource("/web/image"), ".")) }
+   .filter(new Service).run
 }
